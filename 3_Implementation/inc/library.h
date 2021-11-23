488011fe42de431bd6785bdc30fc5d5b4ce9a2e6
DumpalaSaiCharan <94379233+DumpalaSaiCharan@users.noreply.github.com>
2021-11-24 03:15:47 +0530

    updated

--- 3_Implementation/inc/library.h
+++ 3_Implementation/inc/library.h
@@ -1,1 +1,11 @@
 
+
+    Add required header files and function prototypes here
+    upload this file to submitty
+
+ Included to allow users to call functions from math library if required 
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
